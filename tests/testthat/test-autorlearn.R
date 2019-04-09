38cfa14a7aabd0047bcf53f8be46d99353b15c7f
M.Maher <m.maher525@gmail.com>
2019-04-09 14:22:28 +0300

    fix travis_2

--- tests/testthat/test-autorlearn.R
+++ tests/testthat/test-autorlearn.R
@@ -1,13 +1,8 @@
 context("test-autorlearn")
 
 test_that("option1", {
-  result1 <- autoRLearn(1, 'E:/Fdrive/Masters/R/SmartML_Final_V01.00/SmartML/sampleDatasets/shuttle/train.arff', 'E:/Fdrive/Masters/R/SmartML_Final_V01.00/SmartML/sampleDatasets/shuttle/test.arff', option = 1, preProcessF = 'pca', nComp = 4, nModels = 2)
+  result1 <- autoRLearn(1, system.file("extdata", "shuttle/train.arff", package = "SmartML"), system.file("extdata", "shuttle/train.arff", package = "SmartML"), option = 1, preProcessF = 'pca', nComp = 3, nModels = 2)
   result1$clfs  #Vector of recommended nModels classifiers
   result1$params #Vector of initial suggested parameter configurations of nModels recommended classifiers
-})
-
-
-test_that("option2", {
-  result3 <- autoRLearn(0.1, 'E:/Fdrive/Masters/R/SmartML_Final_V01.00/SmartML/sampleDatasets/EEGEyeState/train.csv', 'E:/Fdrive/Masters/R/SmartML_Final_V01.00/SmartML/sampleDatasets/EEGEyeState/test.csv', vRatio = 0.2, selectedFeats = c(1,3,5,7,9,11,13,15), missingOpr = TRUE) # Option 2 runs for both classifier algorithm selection and parameter tuning for
 })
 
